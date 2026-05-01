f35061e5b3af9410f3e2f81ddeaf9d2ad3a6c243
202021008 <105276598+wsc1127@users.noreply.github.com>
2026-05-02 01:50:13 +0900

    Update Comm.h

--- lib/Comm/Comm.h
+++ lib/Comm/Comm.h
@@ -1,9 +1,18 @@
 #pragma once
+
+#include <Arduino.h>
 #include "esp_camera.h"
 
 namespace Comm {
+    // BLE 초기화
     void begin();
+
+    // 카메라로 찍은 JPEG 프레임을 저장하고 전송 준비
+    void setFrame(camera_fb_t* fb);
+
+    // loop()에서 계속 호출하면서 BLE로 이미지 chunk 전송
     void handleClient();
 
-    void setFrame(camera_fb_t* fb);
+    // BLE 연결 여부 확인
+    bool isConnected();
 }
